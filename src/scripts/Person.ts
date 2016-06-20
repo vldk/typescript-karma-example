3ff7ae8c19c61eb8c659983b9b0bdd52ed59011a
vldk <kravchuk.vladimir@gmail.com>
2016-06-20 16:25:02 +0300

    Added new test for Person

--- src/scripts/Person.ts
+++ src/scripts/Person.ts
@@ -8,11 +8,17 @@ export class Person implements IPerson {
 
     firstName:string;
     lastName:string;
+    age:number;
 
-
-    constructor(firstName:string, lastName:string) {
+    constructor(firstName:string, lastName:string, age:number = 0) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.age = age;
+    }
+
+    setAge(age:number){
+        if(age < 0) throw new Error('Invalid age value');
+        this.age = age;
     }
 }
 
--- test/unit/person.spec.ts
+++ test/unit/person.spec.ts
@@ -2,14 +2,34 @@
 import {Person} from "../../src/scripts/Person";
 import {expect} from 'chai';
 
-describe('Person', () => {
+describe('Person', ()=>{
+    let person;
     let first = 'John';
     let last = 'Doe';
-    let person = new Person(first, last);
 
-    it('should have first and last name at creation', () => {
+    beforeEach(()=>{
+        person = new Person(first, last);
+    });
 
+
+    it('should have first and last name at creation', ()=>{
         expect(person.firstName).to.be.eq(first);
         expect(person.lastName).to.be.eq(last);
+    });
+
+    describe('person age', ()=>{
+        it('should have default age', ()=>{
+            expect(person.age).to.be.eq(0);
+        });
+
+        it('should set valid age', ()=>{
+            person.setAge(25);
+            expect(person.age).to.be.eq(25);
+
+            expect( () => {
+                person.setAge(-25)
+            }).to.throw('Invalid age value');
+
+        })
     });
 });
